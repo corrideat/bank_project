e5d17262510269fa4211d352fbedc8d71b1179b1
Verminaard56 <phillipriley@ufl.edu>
2013-04-13 22:24:01 -0400

    Changed core class field declarations and added frame opening.

--- Bank/src/backend/Core.java
+++ Bank/src/backend/Core.java
@@ -3,22 +3,28 @@ package backend;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.JFrame;
+
 import date.DateTime;
 
+import ui.MainFrame;
 import user.User;
 
 import account.Account;
 
 public class Core {
-	protected static DateTime m_dtCurrentTime = new DateTime();
 	
-	protected static Map<Long, Account> m_aaAccounts = new HashMap<Long, Account>();
-	protected static Map<String, User> m_auUsers  = new HashMap<String, User>();
+	//these fields need to be public for the frames to see them since they are in a different package
+	public static DateTime m_dtCurrentTime = new DateTime();
 	
-	protected static double currentCap = 0D; 
+	public static Map<Long, Account> m_aaAccounts = new HashMap<Long, Account>();
+	public static Map<String, User> m_auUsers  = new HashMap<String, User>();
+	
+	public static double currentCap = 0D; 
 
 	public static void main(String[] args) {
-		
+		JFrame loginFrame = new MainFrame();
+		loginFrame.setVisible(true);
 	}
 	
 	public static void save() {
